5dd498c46e29a68f0039f5f9291836447bfa54a3
nehajyoti <nehajyoti04@gmail.com>
2017-03-16 03:57:49 +0530

    Duplicate order issue resolved automatically here.

--- commerce_razorpay.js
+++ commerce_razorpay.js
@@ -11,7 +11,9 @@
       var commerce_order_id = settings.commerce_razorpay.commerce_order_id;
       var payment_id = '';
       var payment_settings = JSON.stringify(settings.commerce_razorpay.payment_settings);
-
+      var billing_address = settings.commerce_razorpay.billing_address;
+      var name = billing_address.first_name + " " + billing_address.last_name;
+      var address = name + " " + billing_address.thoroughfare + " " + billing_address.locality + " " + billing_address.sub_administrative_area + " " + billing_address.country;
 
       var options = {
         // "key": "rzp_test_ipkgumBJtJrvd1",
@@ -22,31 +24,26 @@
         "image": logo,
         "order_id": order_id,
         "handler": function(response) {
-          // alert("payment_settings");
-          // console.log(payment_settings);
-
+          // console.log("response");
+          // console.log(JSON.stringify(response));
+          // exit;
 
           var payment_id = response.razorpay_payment_id;
 
           window.location = '/capture-payment?payment_id=' + payment_id + '&amount=' + amount + '&order_id=' + commerce_order_id + '&payment_settings=' + payment_settings + '&response=' + JSON.stringify(response);
           $('razor-payment-id').val(response.razorpay_payment_id);
-
-
         },
         "prefill": {
-          "name": "Neha",
+          "name": name,
           "email": "neha.jyoti@yahoo.com"
         },
         "notes": {
-          "address": "Hello World"
+          "address": address
         },
         "theme": {
           "color": "#F37254"
         }
       };
-
-
-
 
       var rzp1 = new Razorpay(options);
       rzp1.open();
@@ -56,7 +53,6 @@
       //   rzp1.open();
       //   e.preventDefault();
       // }
-
 
     }
   };
